2a86eba40a2b09e251bdda48bb03c6e17a2b44d4
architect <alex.m3da@gmail.com>
2017-10-30 11:53:54 +0200

    ADD: product list by category

--- warhammer/e_store/urls.py
+++ warhammer/e_store/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ProductListView.as_view(), name='product_list'),
-    url(r'^(?P<pk>\d+)/$', views.ProductDetailView.as_view(), name='product_details'),
+    url(r'^$', views.CategoryListView.as_view(), name='category_list'),
+    url(r'^(?P<pk>\d+)/$', views.ProductListView.as_view(), name='product_list'),
+    url(r'^([0-9]+)/(?P<pk>\d+)/$', views.ProductDetailView.as_view(), name='product_details'),
         ]
--- warhammer/e_store/views.py
+++ warhammer/e_store/views.py
@@ -3,10 +3,21 @@ from django.views.generic import ListView, DetailView
 from e_store.models import *
 
 
-class ProductListView(ListView):
+class CategoryListView(ListView):
+    template_name = 'e_store/category_list.html'
+    model = Category
+    context_object_name = 'categories'
+
+
+class ProductListView(DetailView):
     template_name = 'e_store/product_list.html'
-    model = Product
-    context_object_name = 'products'
+    model = Category
+    context_object_name = 'category'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['products'] = Product.objects.filter(category=context['category'])
+        return context
 
 
 class ProductDetailView(DetailView):
