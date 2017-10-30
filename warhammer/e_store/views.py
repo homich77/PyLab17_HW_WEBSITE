57b48b78304239f460c679ded9b08ff85eabfb7b
architect <alex.m3da@gmail.com>
2017-10-30 10:30:45 +0200

    ADD: product views

--- warhammer/e_store/views.py
+++ warhammer/e_store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from e_store.models import *
 
-# Create your views here.
+
+class ProductListView(ListView):
+    template_name = 'e_store/product_list.html'
+    model = Product
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    template_name = 'e_store/product.html'
+    model = Product
+    context_object_name = 'product'
