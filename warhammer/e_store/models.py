3b0e236a9dbbaa00c83cd573203b93ec13421584
architect <alex.m3da@gmail.com>
2017-10-30 10:05:14 +0200

    ADD: Product and product category models for DB

--- warhammer/e_store/models.py
+++ warhammer/e_store/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    PRODUCT_CATEGORIES = (
+        ('figures', 'figures'),
+        ('terrains', 'terrains'),
+        ('books', 'books'),
+        ('painting', 'painting'),
+        ('digital', 'digital'),
+    )
+    title = models.CharField(max_length=10, choices=PRODUCT_CATEGORIES, default='figures')
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    image = models.ImageField(blank=True, upload_to='images/')
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
